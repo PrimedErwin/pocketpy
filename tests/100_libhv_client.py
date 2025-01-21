a3dbfcbba9720a16fce3d4567feb66a645c0647d
PrimedErwin <1973609350@qq.com>
2025-01-21 20:08:19 +0800

    add interactive test

--- tests/100_libhv_client.py
+++ tests/100_libhv_client.py
@@ -53,19 +53,6 @@ def http_client_test(client: libhv.HttpClient, serv: libhv.HttpServer, url='127.
     wait(resp, serv)
     assert resp.text == 'Successfully deleted content!'
 
-
-# test server start/stop
-a_server = libhv.HttpServer(remote_host, remote_port)
-a_error = a_server.start()
-assert libhv.strerror(a_error) == 'OK'
-a_error = a_server.stop()
-assert libhv.strerror(a_error) == 'OK'
-a_server.start()
-
-# test client
-http_client_test(libhv.HttpClient(), a_server)
-a_server.stop()
-
 # --WebSocket TEST---
 
 # def test funcs for server and client
@@ -82,19 +69,6 @@ def get_channel_id(serv: libhv.HttpServer) -> libhv.WsChannelId:
         channel_id, msg = WsChannel
     return channel_id
 
-def ws_client_2_server_test(client: libhv.WebSocketClient, serv: libhv.HttpServer, channel: libhv.WsChannelId,
-                            data='Hello from client') -> None:
-    send_result = client.send(data)
-    assert send_result > 0
-    recv_serv = serv.ws_recv()
-    while recv_serv is None:
-        recv_serv = serv.ws_recv()
-    if recv_serv[0] == 'onmessage':
-        assert recv_serv[1][0] == channel
-        assert recv_serv[1][1] == data
-    elif recv_serv[0] == 'onclose':
-        assert recv_serv[1] == channel
-        serv.ws_close(channel)
 
 def ws_server_2_client_test(serv: libhv.HttpServer, client: libhv.WebSocketClient, channel: libhv.WsChannelId,
                             data='Hello from server') -> None:
@@ -106,35 +80,13 @@ def ws_server_2_client_test(serv: libhv.HttpServer, client: libhv.WebSocketClien
     if recv_client[0] == 'onmessage':
         assert recv_client[1] == data
 
-# def websocket ping interval and start
-a_server.ws_set_ping_interval(1000)
-a_server.start()
-
 if INTERACTIVE_TEST:
-    channel = get_channel_id(a_server)
-    print('Connected channel = ',channel)
-    while 1:
-        mi = input()
-        retval = a_server.ws_send(channel, 'Test'+mi)
-        print('server sent status = ',retval)
-        retmsg = a_server.ws_recv()
-        print(retmsg)
-else:
     # test WebSocket client start
     a_client = libhv.WebSocketClient()
     a_error = a_client.open(ws_client_url)
     assert libhv.strerror(a_error) == 'OK'
 
     # test client to server msg
-    channel = get_channel_id( a_server)
-    ws_client_2_server_test(a_client, a_server, channel)
-    # test server to client msg
-    ws_server_2_client_test(a_server, a_client, channel)
-
-    # close client and server
-    a_error = a_client.close()
-    assert libhv.strerror(a_error) == 'OK'
-    a_error = a_server.stop()
-    assert libhv.strerror(a_error) == 'OK'
-
-    print(libhv.strerror(19))
+    while 1:
+        mi = input()
+        a_client.send('Test'+mi)
