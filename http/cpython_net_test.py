c4958d610d6c3b580c0dc8294f626d2593e60290
PrimedErwin <1973609350@qq.com>
2025-01-23 19:41:37 +0800

    add concurrent test

--- http/cpython_net_test.py
+++ http/cpython_net_test.py
@@ -21,35 +21,92 @@ def ansi_fg(color: tuple[int, int, int], text: str):
 
 client_list: list[subprocess.Popen] = []
 
+def cpython_http_sync() -> int:
+    cpython_http_client = cmd('python .\\cpython_http_client.py')
+    client_list.append(cpython_http_client)
+    outs, _ = cpython_http_client.communicate()
+    return outs
+
+def pkpy_http_sync() -> int:
+    pkpy_http_client = cmd('..\\main.exe .\\pkpy_http_client.py')
+    client_list.append(pkpy_http_client)
+    outs, _ = pkpy_http_client.communicate()
+    return outs
+
+def cpython_ws_sync() -> int:
+    cpython_ws_client = cmd('python .\\cpython_ws_client.py')
+    client_list.append(cpython_ws_client)
+    outs, _ = cpython_ws_client.communicate()
+    return outs
+
+def pkpy_ws_sync() -> int:
+    pkpy_ws_client = cmd('..\\main.exe .\\pkpy_ws_client.py')
+    client_list.append(pkpy_ws_client)
+    outs, _ = pkpy_ws_client.communicate()
+    return outs
+
+def cpython_http_concurrent(thread_num: int) -> list[int]:
+    ret_list: list[int] = []
+    concurrent_list: list[subprocess.Popen] = []
+    for i in range(thread_num):
+        concurrent_list.append(cmd('python .\\cpython_http_client.py'))
+    for i in range(thread_num):
+        outs, _ = concurrent_list[i].communicate()
+        ret_list.append(outs)
+    return ret_list
+
+def pkpy_http_concurrent(thread_num: int) -> list[int]:
+    ret_list: list[int] = []
+    concurrent_list: list[subprocess.Popen] = []
+    for i in range(thread_num):
+        concurrent_list.append(cmd('..\\main.exe .\\pkpy_http_client.py'))
+    for i in range(thread_num):
+        outs, _ = concurrent_list[i].communicate()
+        ret_list.append(outs)
+    return ret_list
+
+def cpython_ws_concurrent(thread_num: int) -> list[int]:
+    ret_list: list[int] = []
+    concurrent_list: list[subprocess.Popen] = []
+    for i in range(thread_num):
+        concurrent_list.append(cmd('python .\\cpython_ws_client.py'))
+    for i in range(thread_num):
+        outs, _ = concurrent_list[i].communicate()
+        ret_list.append(outs)
+    return ret_list
+
+def pkpy_ws_concurrent(thread_num: int) -> list[int]:
+    ret_list: list[int] = []
+    concurrent_list: list[subprocess.Popen] = []
+    for i in range(thread_num):
+        concurrent_list.append(cmd('..\\main.exe .\\pkpy_ws_client.py'))
+    for i in range(thread_num):
+        outs, _ = concurrent_list[i].communicate()
+        ret_list.append(outs)
+    return ret_list
+
+
 # start cpython server
 print('Starting server...', '#'*30, sep='\n')
 print('SERVER MODE: ', ansi_fg(blue, 'CPYTHON'))
 cpython_serv = cmd_silent('python .\\cpython_server.py')
 
-cpython_http_client = cmd('python .\\cpython_http_client.py')
-outs, _ = cpython_http_client.communicate()
-cpython_http_client.wait()
-# print(ansi_fg(blue, 'HTTP  CPYTHON'), 'client->server 1 sec: ', ansi_fg(green, outs))
-
-cpython_http_client = cmd('python .\\cpython_http_client.py')
-client_list.append(cpython_http_client)
-outs, _ = cpython_http_client.communicate()
+outs = cpython_http_sync()
 print(ansi_fg(blue, 'HTTP  CPYTHON'), 'client->server 1 sec: ', ansi_fg(green, outs))
 
-pkpy_http_client = cmd('..\\main.exe .\\pkpy_http_client.py')
-client_list.append(pkpy_http_client)
-outs, _ = pkpy_http_client.communicate()
+outs = pkpy_http_sync()
 print(ansi_fg(blue, 'HTTP  PKPY'), 'client->server 1 sec: ', ansi_fg(green, outs))
 
-cpython_ws_client = cmd('python .\\cpython_ws_client.py')
-client_list.append(cpython_ws_client)
-outs, _ = cpython_ws_client.communicate()
+outs = cpython_ws_sync()
 print(ansi_fg(blue, 'WebSocket  CPYTHON'), 'client->server 1 sec: ', ansi_fg(green, outs))
 
-pkpy_ws_client = cmd('..\\main.exe .\\pkpy_ws_client.py')
-client_list.append(pkpy_ws_client)
-outs, _ = pkpy_ws_client.communicate()
+outs = pkpy_ws_sync()
 print(ansi_fg(blue, 'WebSocket PKPY'), 'client->server 1 sec: ', ansi_fg(green, outs))
+
+print(cpython_http_concurrent(4))
+print(pkpy_http_concurrent(4))
+print(cpython_ws_concurrent(4))
+print(pkpy_ws_concurrent(4))
 
 print('Waiting clients shutting down...\n')
 while client_list:
@@ -68,29 +125,22 @@ print('Starting server...', '#'*30, sep='\n')
 print('SERVER MODE: ', ansi_fg(blue, 'PKPY'))
 pkpy_serv = cmd_silent('..\\main.exe .\\pkpy_server.py')
 
-cpython_http_client = cmd('python .\\cpython_http_client.py')
-outs, _ = cpython_http_client.communicate()
-# print(ansi_fg(blue, 'HTTP  CPYTHON'), 'client->server 1 sec: ', ansi_fg(green, outs))
-
-cpython_http_client = cmd('python .\\cpython_http_client.py')
-outs, _ = cpython_http_client.communicate()
-client_list.append(cpython_http_client)
+outs = cpython_http_sync()
 print(ansi_fg(blue, 'HTTP  CPYTHON'), 'client->server 1 sec: ', ansi_fg(green, outs))
 
-pkpy_http_client = cmd('..\\main.exe .\\pkpy_http_client.py')
-outs, _ = pkpy_http_client.communicate()
-client_list.append(pkpy_http_client)
+outs = pkpy_http_sync()
 print(ansi_fg(blue, 'HTTP  PKPY'), 'client->server 1 sec: ', ansi_fg(green, outs))
 
-cpython_ws_client = cmd('python .\\cpython_ws_client.py')
-outs, _ = cpython_ws_client.communicate()
-client_list.append(cpython_ws_client)
+outs = cpython_ws_sync()
 print(ansi_fg(blue, 'WebSocket  CPYTHON'), 'client->server 1 sec: ', ansi_fg(green, outs))
 
-pkpy_ws_client = cmd('..\\main.exe .\\pkpy_ws_client.py')
-outs, _ = pkpy_ws_client.communicate()
-client_list.append(pkpy_ws_client)
+outs = pkpy_ws_sync()
 print(ansi_fg(blue, 'WebSocket PKPY'), 'client->server 1 sec: ', ansi_fg(green, outs))
+
+print(cpython_http_concurrent(4))
+print(pkpy_http_concurrent(4))
+print(cpython_ws_concurrent(4))
+print(pkpy_ws_concurrent(4))
 
 print('Waiting clients shutting down...')
 while client_list:
